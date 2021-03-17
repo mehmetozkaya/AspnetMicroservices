8f5e4c0d874c14fadfc04632a4f4094c4174174f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-03-17 13:12:51 +0300

    dis api

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,11 +1,6 @@
+using Discount.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Discount.API
 {
@@ -13,7 +8,9 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
