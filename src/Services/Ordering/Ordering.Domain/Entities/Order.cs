0860a9fbdf0f7df66cb5155ceca974a96af0e448
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-03-18 15:50:48 +0300

    layers

---
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,31 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // BillingAddress
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
