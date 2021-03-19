f0bf4e68a974529fc69890161eba5da0a28fa197
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-03-19 11:51:38 +0300

    ordering application

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
